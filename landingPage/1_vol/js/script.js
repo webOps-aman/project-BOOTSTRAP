0a90b4b105dd111022e27c27b21f0b7bc6170ee9
webOps-aman <amanhxe@gmail.com>
2024-12-15 01:15:01 +0530

    update files 1_vol

---
+++ landingPage/1_vol/js/script.js
@@ -0,0 +1,25 @@
+// -----auto text start here
+let auto_text = document.querySelector(".auto-text");
+		let careers = ['Company Name'];
+		let careerIndex = 0;
+		let characterIndex = 0;
+
+		function updateText(){
+			
+			characterIndex++;
+			auto_text.innerHTML = `${careers[careerIndex].slice(0, characterIndex)}`;
+
+			if(characterIndex === careers[careerIndex].length){
+				careerIndex++;
+				characterIndex = 0
+			}
+
+			if(careerIndex === careers.length){
+				careerIndex = 0;
+			}
+
+			setTimeout(updateText, 300);
+			
+		}
+		updateText()
+// -----auto text end here
